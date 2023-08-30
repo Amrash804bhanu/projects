dc97213ad8ec812166f7ff99c45c665e68c3359c
Amrash804bhanu <122105050+Amrash804bhanu@users.noreply.github.com>
2023-08-30 21:30:55 +0530

    Add files via upload

---
+++ ejs-assignment/ejs1.js
@@ -0,0 +1,14 @@
+var a=10; 
+var b=8; // Here value of b is 8
+var c=6;
+
+{
+    let a = 2;
+    var b = 10;
+    const c = 8;
+  c=5;
+}
+document.write("The value of a is : " +a);
+
+document.write("<br>");
+document.write("The value of b is : " +b);1

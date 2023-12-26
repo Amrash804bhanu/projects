caf307295a019b2337576c940c8a0c8a8702a898
Amrash804bhanu <122105050+Amrash804bhanu@users.noreply.github.com>
2023-12-26 21:50:38 +0530

    Add files via upload

---
+++ quora-clone project/javascript.js
@@ -0,0 +1,19 @@
+// to make the question tag display display what we should do 
+
+
+
+ 
+function opening(){
+    let questionWindow = document.getElementById('question-popup');
+let open = document.getElementById('open-key').click();
+questionWindow.style.display = 'block';
+
+
+}
+
+function closing() {
+    let questionWindow = document.getElementById('question-popup');
+     let close = document.getElementById('close-key').click();
+    questionWindow.style.display ='none';
+}
+
